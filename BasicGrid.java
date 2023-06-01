6ec9e553035e661c410f1b0bd74444dd29766fd2
Sam Carpenter <135187486+TheLynxOfNine@users.noreply.github.com>
2023-06-01 11:37:09 +0800

    Create BasicGrid.java
    Write a Java program to print the following grid.
    
    Expected Output :
    
    - - - - - - - - - -                                                                                           
    - - - - - - - - - -                                                                                           
    - - - - - - - - - -                                                                                           
    - - - - - - - - - -                                                                                           
    - - - - - - - - - -                                                                                           
    - - - - - - - - - -                                                                                           
    - - - - - - - - - -                                                                                           
    - - - - - - - - - -                                                                                           
    - - - - - - - - - -                                                                                           
    - - - - - - - - - -

---
+++ BasicGrid.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+                System.out.print("- ");
+            }
+            System.out.println(" ");
+        }
+    } 
+}
