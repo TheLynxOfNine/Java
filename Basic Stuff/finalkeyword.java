4dabfbf42d18d1bc66c63c07f17ab00370b1260d
Sam Carpenter <135187486+TheNineLynx@users.noreply.github.com>
2023-06-16 18:18:30 +0800

    Create finalkeyword.java

---
+++ Basic Stuff/finalkeyword.java
@@ -0,0 +1,11 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+
+        final double pi = 3.1415; // final you cannot put another "pi" var
+        // pi = 4;
+
+        System.out.println(pi);
+
+    }
+}
