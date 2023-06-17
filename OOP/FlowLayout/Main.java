212bd2ba9950ceda3ce6ce96de632df55c00c967
Sam Carpenter <135187486+TheNineLynx@users.noreply.github.com>
2023-06-17 17:23:36 +0800

    Create Main.java

---
+++ OOP/FlowLayout/Main.java
@@ -0,0 +1,12 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+
+        // FlowLayout - places components in a row, sized at their preferred size
+        //              if the horizontal space in the container is too small,
+        //              the flowlayout class uses the next available row
+
+        new MyFrame();
+
+    }
+}
